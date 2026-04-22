4c78d0ead50ae67979e8c02e7ecb70fca33fdb7b
Valery Polishchuk <valera.polishyk2@gmail.com>
2026-04-22 22:40:10 +0300

    add new infrastructure layers

--- src/Sentia.API/Program.cs
+++ src/Sentia.API/Program.cs
@@ -6,10 +6,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SentiaDatabase")));
 
 builder.Services.AddIdentityCore<ApplicationUser>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddSignalR()
+    .AddAzureSignalR(builder.Configuration.GetConnectionString("AzureSignalR"));
 
 var app = builder.Build();
 
